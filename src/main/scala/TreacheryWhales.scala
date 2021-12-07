886ffb99bd28604e97bc5b419f8c17fae745144d
Jesus Montero Luque <jesus.montero@ing.com>
2021-12-07 13:59:04 +0100

    TreacheryWhales refactor

--- src/main/scala/TreacheryWhales.scala
+++ src/main/scala/TreacheryWhales.scala
@@ -3,14 +3,16 @@ object TreacheryWhales {
   def calculateFuel(positions: List[Int]) =
     positions.map { next =>
       positions.foldLeft(0) { (acc,n) =>
-        acc + (next - n) * (if (next < n) -1 else 1)
+        acc + subtract(next, n)
       }
     }.min
 
   def calculateFuel2(positions: List[Int]) =
     (0 to positions.max).map { next =>
       positions.foldLeft(0) { (acc, n) =>
-        acc + (1 to ((next - n) * (if (next < n) -1 else 1))).sum
+        acc + (1 to subtract(next, n)).sum
       }
     }.min
+
+  def subtract(x: Int, y: Int) = (x - y) * (if (x < y) -1 else 1)
 }
