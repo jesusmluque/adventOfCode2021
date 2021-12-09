39740e42a5b2ceb71a8767afb6fe9d6abf28fd83
Jesus Montero Luque <jesus.montero@ing.com>
2021-12-09 18:34:15 +0100

    9th day: Bmoke Basin finished

--- src/main/scala/SmokeBasin.scala
+++ src/main/scala/SmokeBasin.scala
@@ -1,20 +1,70 @@
+import scala.annotation.tailrec
+
 object SmokeBasin {
 
-  case class SmokeBasinField(vector: Vector[Int], rowSize: Int)
+  case class SmokeBasinField(private val field: Vector[Vector[Int]]) {
+    def get(coordinates: (Int, Int)): Int = field(coordinates._1)(coordinates._2)
+
+    def getNeighbors(coordinates: (Int, Int)): List[(Int, Int)] =
+      val indexLastXElement = field.size - 1
+      val indexLastYElement = field.head.size - 1
+      coordinates match {
+        case (0, 0) => (0, 1) :: (1, 0) :: Nil
+        case (0, y) if y == indexLastYElement => (1, indexLastYElement) :: (0, indexLastYElement - 1) :: Nil
+        case (0, y) => (1, y) :: (0, y - 1) :: (0, y + 1) :: Nil
+        case (x, 0) if x == indexLastXElement => (indexLastXElement, 1) :: (indexLastXElement - 1, 0) :: Nil
+        case (x, 0) => (x + 1, 0) :: (x - 1, 0) :: (x, 1) :: Nil
+        case (x, y) if x == indexLastXElement && y == indexLastYElement => (indexLastXElement, indexLastYElement - 1) :: (indexLastXElement - 1, indexLastYElement) :: Nil
+        case (x, y) if x == indexLastXElement => (indexLastXElement, y - 1) :: (indexLastXElement, y + 1) :: (indexLastXElement - 1, y) :: Nil
+        case (x, y) if y == indexLastYElement => (x - 1, indexLastYElement) :: (x + 1, indexLastYElement) :: (x, indexLastYElement - 1) :: Nil
+        case (x, y) => (x + 1, y) :: (x - 1, y) :: (x, y + 1) :: (x, y - 1) :: Nil
+      }
+
+    def foldField[A](acc: A)(f: (A, Int, Int) => A) =
+      this.field.zipWithIndex.foldLeft(acc) { (acc1, row) =>
+        row._1.zipWithIndex.foldLeft(acc1) { (acc2, point) =>
+          f(acc2, row._2, point._2)
+        }
+      }
+  }
   object SmokeBasinField {
     def apply(raw: List[String]):SmokeBasinField =
-      SmokeBasinField(raw.foldLeft(Vector[Int]())((acc,row) => row.split("").toVector.map(_.toInt) ++ acc), raw.head.length)
+      SmokeBasinField(raw.zipWithIndex.foldLeft(Vector.fill(raw.length)(Vector.fill(raw.head.length)(0)))((acc,row) => acc.updated(row._2, row._1.split("").map(_.toInt).toVector)))
   }
 
   def findRiskLevelFor(input: List[String]) =
     val field = SmokeBasinField(input)
-    field.vector.zipWithIndex.foldLeft(0) { (acc, smoke) =>
-      if ((smoke._2 % field.rowSize - 1 > 0 && field.vector(smoke._2 - 1) < smoke._1) &&
-        (smoke._2 % field.rowSize + 1 < field.rowSize && field.vector(smoke._2 + 1) < smoke._1)  &&
-        (smoke._2 - field.rowSize > 0   && field.vector(smoke._2 - field.rowSize) < smoke._1) &&
-          (smoke._2 - field.rowSize > field.vector.length  && field.vector(smoke._2 + field.rowSize) < smoke._1)) {
-        acc + smoke._1 + 1
-      } else
+    field.foldField(0) {(acc:Int, x:Int, y:Int) =>
+      if (isALowPoint(x, y, field))
+        field.get(x, y) + 1 + acc
+      else
         acc
     }
+
+  def findProductOfThreeLargestBasinsFor(input: List[String]) =
+    def findBasinMembers(field: SmokeBasinField, basin: (Int, Int), acc: Set[(Int, Int)]): Set[(Int, Int)] =
+      if (acc.contains(basin))
+        acc
+      else if (field.get(basin) == 9)
+        acc
+      else
+        field.getNeighbors(basin).foldLeft(acc) { (acc2, p) =>
+          if (field.get(p) != 9)
+            findBasinMembers(field, p, acc2 + basin)
+          else
+            acc2
+        }
+    val field = SmokeBasinField(input)
+    val basins = field.foldField(List[(Int, Int)]()) { (acc, x, y) =>
+      if (isALowPoint(x, y, field))
+        (x, y) :: acc
+      else
+        acc
+    }
+    basins.map(findBasinMembers(field, _, Set())).sortBy(_.size).takeRight(3).map(_.size).product
+
+  def isALowPoint(x: Int, y: Int, field: SmokeBasinField) = {
+    val valuesOfNeighbors = field.getNeighbors((x, y)).map(field.get)
+    valuesOfNeighbors.count(_ <= field.get(x,y)) == 0
+  }
 }
--- src/test/scala/SmokeBasinTest.scala
+++ src/test/scala/SmokeBasinTest.scala
@@ -7,4 +7,16 @@ class SmokeBasinTest extends AnyFlatSpec {
   "The sum of the risk level for the example " should " be 15 " in {
     assert(SmokeBasin.findRiskLevelFor(Source.fromResource("smokeBasin1").getLines().toList) == 15)
   }
+
+  "The sum of the risk level for the exercise " should " be 508 " in {
+    assert(SmokeBasin.findRiskLevelFor(Source.fromResource("smokeBasin2").getLines().toList) == 508)
+  }
+
+  "The product of the size of the three largest basins on the example " should " be 1134 " in {
+    assert(SmokeBasin.findProductOfThreeLargestBasinsFor(Source.fromResource("smokeBasin1").getLines().toList) == 1134)
+  }
+
+  "The product of the size of the three largest basins on the exercise " should " be 1564640 " in {
+    assert(SmokeBasin.findProductOfThreeLargestBasinsFor(Source.fromResource("smokeBasin2").getLines().toList) == 1564640)
+  }
 }
