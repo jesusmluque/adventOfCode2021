1e05f968c20edab59b3102c55f99bd684a8e0c43
Jesus Montero Luque <jesus.montero@ing.com>
2021-12-15 21:08:17 +0100

    14th ExtendedPolymerization - segunda vez

--- src/main/scala/ExtendedPolymerization.scala
+++ src/main/scala/ExtendedPolymerization.scala
@@ -17,6 +17,7 @@ object ExtendedPolymerization {
     val finalTemplate = (1 to steps).foldLeft(templateByChar) { (acc, step) =>
       buildExpansion(acc.head, acc.tail, List())
     }
+    System.out.println(finalTemplate)
     val occ = finalTemplate.foldLeft(Map[String, Long]()) { (acc, n) =>
       if (acc.contains(n))
         acc.updated(n, acc(n) + 1)
@@ -24,4 +25,29 @@ object ExtendedPolymerization {
         acc.updated(n, 1)
     }
     occ.maxBy(_._2)._2 - occ.minBy(_._2)._2
+
+  def getDifferenceMostAndLessOccurrencies2(template: String, pairInsertionRawRules: List[String], steps: Int) =
+    val templateCharList = template.split("").toList
+
+    val pairInsertionRules = pairInsertionRawRules.map { rawRule =>
+      val rawRuleSplit = rawRule.split(" -> ")
+      (rawRuleSplit(0), rawRuleSplit(1))
+    }.toMap
+
+    val templatePairs = templateCharList.tail.foldLeft((templateCharList.head, Map[(String, String), Long](), Map[String, Long](templateCharList.head -> 1L)))  { (acc, c) =>
+      (c, acc._2.updated((acc._1, c), acc._2.getOrElse((acc._1, c), 0L) + 1L), acc._3.updated(c, acc._3.getOrElse(c, 0L) + 1L))
+    }
+
+
+    val totalPairs = (1 to steps).foldLeft((templatePairs._2, templatePairs._3)) { (acc, step) =>
+      acc._1.foldLeft((Map[(String, String), Long](), acc._2)) { (acc2, oldPair) =>
+        val elementToInsert = pairInsertionRules(oldPair._1._1 + oldPair._1._2)
+
+        (acc2._1.updated((oldPair._1._1, elementToInsert), acc2._1.getOrElse((oldPair._1._1, elementToInsert), 0L) + oldPair._2)
+          .updated((elementToInsert, oldPair._1._2), acc2._1.getOrElse((elementToInsert, oldPair._1._2), 0L) + oldPair._2), acc._2.updated(elementToInsert, acc._2.getOrElse(elementToInsert, 0L) + oldPair._2))
+      }
+    }
+    System.out.println(totalPairs)
+
+    totalPairs._2.maxBy(_._2)._2 - totalPairs._2.minBy(_._2)._2
 }
--- src/test/scala/ExtendedPolymerizationTest.scala
+++ src/test/scala/ExtendedPolymerizationTest.scala
@@ -8,20 +8,20 @@ class ExtendedPolymerizationTest extends AnyFlatSpec {
     val raw = Source.fromResource("ExtendedPolymerization1").getLines().toList
     val template = raw.take(raw.indexOf("")).head
     val pairInsertionRawRules = raw.takeRight(raw.size - 1 - raw.indexOf(""))
-    assert(ExtendedPolymerization.getDifferenceMostAndLessOccurrencies(template, pairInsertionRawRules, 10) == 1588L)
+    assert(ExtendedPolymerization.getDifferenceMostAndLessOccurrencies2(template, pairInsertionRawRules, 4) == 1588L)
   }
 
   "After applying 10 steps, the result of taking the quantity of the most common element ans substracting the less common " should " be 3230 for the exercise" in {
     val raw = Source.fromResource("ExtendedPolymerization2").getLines().toList
     val template = raw.take(raw.indexOf("")).head
     val pairInsertionRawRules = raw.takeRight(raw.size - 1 - raw.indexOf(""))
-    assert(ExtendedPolymerization.getDifferenceMostAndLessOccurrencies(template, pairInsertionRawRules, 10) == 3230L)
+    assert(ExtendedPolymerization.getDifferenceMostAndLessOccurrencies2(template, pairInsertionRawRules, 10) == 3230L)
   }
 
   "After applying 40 steps, the result of taking the quantity of the most common element ans substracting the less common " should " be 2188189693529 for the example " in {
     val raw = Source.fromResource("ExtendedPolymerization1").getLines().toList
     val template = raw.take(raw.indexOf("")).head
     val pairInsertionRawRules = raw.takeRight(raw.size - 1 - raw.indexOf(""))
-    assert(ExtendedPolymerization.getDifferenceMostAndLessOccurrencies(template, pairInsertionRawRules, 40) == 2188189693529L)
+    assert(ExtendedPolymerization.getDifferenceMostAndLessOccurrencies2(template, pairInsertionRawRules, 40) == 2188189693529L)
   }
 }
